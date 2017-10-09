62dbecbaf52d443a5f1b605844ac960316a727f8
root <707382092@qq.com>
2017-10-09 17:23:06 +0800

    Quartz

---
+++ quartzStart/src/main/java/com/itheima/jobs/EmailJob.java
@@ -0,0 +1,17 @@
+package com.itheima.jobs;
+
+import java.util.Date;
+
+/**  
+ * ClassName:MyJob <br/>  
+ * Function:  <br/>  
+ * Date:     2017年10月9日 下午3:28:11 <br/>       
+ */
+public class EmailJob {
+    
+    public void sendEmail(){
+      System.out.println("发邮件 "+new Date().toLocaleString());
+    }
+  
+}
+  
---
+++ quartzStart/src/main/java/com/itheima/quartz/MyTest.java
@@ -0,0 +1,17 @@
+package com.itheima.quartz;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**  
+ * ClassName:MyTest <br/>  
+ * Function:  <br/>  
+ * Date:     2017年10月9日 下午4:01:54 <br/>       
+ */
+public class MyTest {
+  
+  
+    public static void main(String[] args) {
+      new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
+}
+  
